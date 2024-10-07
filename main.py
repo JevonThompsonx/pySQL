5f0cd09e7ad42656e9f225428b4ae317772a0109
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-10-07 14:33:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,32 @@ memCursor = mem.cursor()
 
 # Table creation
 
-cursor.execute(""" CREATE TABLE people(
+cursor.execute(""" CREATE TABLE IF NOT EXISTS people(
                first_name TEXT,
                last_name TEXT,
-               age INTEGER,
-)
+               age INTEGER
+);
                """)
+#cursor.execute("""
+ #   INSERT INTO people(first_name,last_name, age)
+  # VALUES("Samantha", "Grey", 21);
+   #            """)
+#secondInsert = ("Johnny", "Karcol", 34)
+#cursor.execute("""
+ #   INSERT INTO people(first_name,last_name, age)
+  #  VALUES(?,?,?);
+   #            """, secondInsert )
+#manyInsert = [
+    #    {"first_name": "Samantha","age":25, "last_name":"Barnes"},
+   #     {"first_name": "Joe","age":35, "last_name":"Barnes"},
+  #      {"first_name": "Sarah","age":25, "last_name":"Barnes"}
+     #   ]
+#cursor.executemany("""
+              #     INSERT INTO people(first_name,last_name,age)
+               #    VALUES(:first_name,:last_name,:age)
+                #   """,manyInsert)
+response = cursor.execute("SELECT DISTINCT last_name FROM people ;").fetchall()
+for person in response:
+    print(person)
+conn.commit()
+conn.close()
