c4a835703007bf57df53a10be39f06163fd25f01
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-10-07 14:33:35 -0700

    Added bun w/ typescript
    I need to make sure at the end of this that I know how to use sql w/ python and typescript. I'll go through the basics w/ python as a simple language to use as well as to give myself practice so my python stays sharp and also because it has jupyter notebooks and I'm still figuring out how to run javascript code w/ that

--- index.ts
+++ index.ts
@@ -4,9 +4,21 @@ import { Database } from 'bun:sqlite'
   using mem = new Database(':memory:')
 
   db.query(`
-           CREATE TABLE person(
-             name TEXT,
-             age INTEGER,
+           CREATE TABLE IF NOT EXISTS people(
+             first_name TEXT,
+             last_name TEXT,
+             age INTEGER
            )
            `).run()
+  const result = db.query(`
+         SELECT DISTINCT first_name, last_name,age FROM people
+         `).all()
+  for (let row of result) {
+    console.log(`
+  first name ${row.first_name}
+  last_name ${row.last_name}
+  age ${row.age}
+                `)
+  }
 }
+
